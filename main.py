43162d20039735efdd8464dcfea408cde700b614
Prabhudatta Barik <thexas21@gmail.com>
2023-12-18 12:17:42 +0530

    Added  dotenv

--- main.py
+++ main.py
@@ -3,28 +3,28 @@ from bs4 import BeautifulSoup
 import requests
 import spotify
 from spotipy.oauth2 import SpotifyOAuth
-# from dotenv import load_dotenv
-# import os
-#
-#
-# def configure():
-#     load_dotenv()
+from dotenv import load_dotenv
+import os
 
 
+def configure():
+    load_dotenv()
+
+
+# Scraping BillBoard Hindi
 date = input("Which year do you want to go back(YYYY-MM-DD):")
-
 response = requests.get("https://www.billboard.com/charts/india-songs-hotw/" + date)
-
 soup = BeautifulSoup(response.text, 'html.parser')
 song_names_span = soup.select("li ul li h3")
 song_names = [song.getText().strip() for song in song_names_span]
 
+# Authentication
 sp = spotipy.Spotify(
     auth_manager=SpotifyOAuth(
         scope="playlist-modify-private",
         redirect_uri="https://example.com",
-        client_id="a1976213bd974e48af8ca396c046cd72",
-        client_secret="be3afac433c342048634ea019cc95003",
+        client_id={os.getenv('CLIENT_ID')},
+        client_secret={os.getenv('CLIENT_SECRET')},
         show_dialog=True,
         cache_path="token.txt",
         username="Prabhu",
